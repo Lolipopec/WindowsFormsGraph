883183617cadcc2fdbb0242616c85529152eca69
павел Власов <pacha_vlasov_95@mail.ru>
2022-09-21 18:32:48 +0300

    Пу пу пу

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Media;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+using Brushes = System.Windows.Media.Brushes;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf.Charts.Base;
 
 namespace WindowsFormsApp1
 {
@@ -19,23 +22,23 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-
         }
         private void CartesianChart1OnDataClick(object sender, ChartPoint chartPoint)
         {
             MessageBox.Show("You clicked (" + chartPoint.X + "," + chartPoint.Y + ")");
         }
 
+        public double[,] SumSin()
+        {
+            double[,] val = new double[2, 5] { { 0, 1, 0, 1, 0 }, { -5, -4, -3, -2, -1 } };//Массив значений {-5, -4, -3,-2,-1}-Ox { 0, 1 ,0,1,0 }-Oy
+            return val;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            //if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
-                build_Graph();
-            //else
-            //{
-            //    MessageBox.Show("Заполни!!!!!!!!!!!!");
-            //}
+            build_Graph(SumSin());
         }
-        private void build_Graph()
+        private void build_Graph(double[,] val)
         {
             //Очистка предыдущих коллекций
             cartesianChart1.Series.Clear();
@@ -44,19 +47,11 @@ namespace WindowsFormsApp1
 
             SeriesCollection series = new SeriesCollection();//Коллекция линий
             LineSeries ln = new LineSeries();//Линия
-            //ln.ChartPoints//Посмотреть
-            ln.Title = "";
-            int[,] val = new int[2, 5] { { 0, 1 ,0,1,0 }, {-5, -4, -3,-2,-1} };//Массив значений {-5, -4, -3,-2,-1}-Ox { 0, 1 ,0,1,0 }-Oy
-            ChartValues<int> Values = new ChartValues<int>();//Коллекция значений по Oy
-            //Переписали из массива значения по Oy
-            for (int i = 0; i < 1; i++)
+         
+            ChartValues<ObservablePoint> Values = new ChartValues<ObservablePoint>();//Коллекция значений по Oy
+            for (int j = 0; j < val.GetLength(1); j++)
             {
-                for (int j = 0; j < val.GetLength(1); j++)
-                {
-                    
-                    Values.Add(val[i,j]);
-                }
-
+                Values.Add(new ObservablePoint(val[1, j], val[0, j]));
             }
             ln.Values = Values;//Добавление значений на линию
             series.Add(ln);//Добавление линии в коллекцию линий
@@ -64,58 +59,55 @@ namespace WindowsFormsApp1
             //Определение максимума и минимума по Ox
             double min = val[1, 0];
             double max = val[1, 0];
-            for (int i = 1; i < 2; i++)
+            for (int j = 0; j < val.GetLength(1); j++)
             {
-                for (int j = 0; j < val.GetLength(1); j++)
+                if (min > val[1, j])
                 {
-                    if (min > val[i, j])
-                    {
-                        min = val[i, j];
-                    }
-                    if (max < val[i, j])
-                    {
-                        max = val[i, j];
-                    }
+                    min = val[1, j];
                 }
-            }
-            //Определили точки по оси Ox
-            List<double> values = new List<double>();
-            double step = (max - min) / (val.GetLength(1) - 1);
-            for (int i = 0; i < val.GetLength(1); i++)
-            {
-               
-                values.Add(min);
-                min += step;
-            }
-            //Записали в лист стринг для создания оси
-            List<string> l = new List<string>();
-            for (int i = 0; i <values.Count; i++)
-            {
-                l.Add(values[i].ToString());
+                if (max < val[1, j])
+                {
+                    max = val[1, j];
+                }
             }
             //Ось Ox
             cartesianChart1.AxisX.Add(new Axis
             {
                 Title = "X",//подпись
-                Labels = l,//значения на оси
+                LabelFormatter = value => value.ToString(""),
+                MinValue = min,
+                MaxValue = max,
                 Separator = new Separator
                 {
                     StrokeThickness = 1,
                     Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
                 }
             });
+            min = val[0, 0];
+            max = val[0, 0];
+            for (int j = 0; j < val.GetLength(1); j++)
+            {
+                if (min > val[0, j])
+                {
+                    min = val[0, j];
+                }
+                if (max < val[0, j])
+                {
+                    max = val[0, j];
+                }
+            }
             //Ось Oy
             cartesianChart1.AxisY.Add(new Axis
             {
                 Title = "Y",
-                Labels = new[] { "-1", "0", "1"},
+                MinValue = min,
+                MaxValue = max,
                 Separator = new Separator
                 {
                     StrokeThickness = 1,
                     Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
                 }
             });
-
             cartesianChart1.LegendLocation = LegendLocation.Bottom;//Где расположить легенду графика
 
 
